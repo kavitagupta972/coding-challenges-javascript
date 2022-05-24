93a4e031386f27bfd0d83842a5b56af4909032ad
kavitagupta972 <kavitagupta972@gmail.com>
2022-05-24 13:44:28 +0530

    Initial commit - coding challenge day1

---
+++ app.js
@@ -0,0 +1,26 @@
+//First Way 
+function inbuiltReverse(str) {
+    
+    return str.split('').reverse().join('');
+    console.log("Reveresed ");
+}
+
+console.log(inbuiltReverse("coding challenge"));
+console.log(inbuiltReverse("sharetoolearn"));
+console.log(inbuiltReverse("1234"));
+
+//Second Way
+function concatReverse(str){
+    let reversed = '';
+    
+    for(let ch of str) {
+        reversed = ch+reversed;
+    }
+    
+    return reversed;
+}
+
+
+console.log(concatReverse("coding challenge"));
+console.log(concatReverse("sharetoolearn"));
+console.log(concatReverse("1234"));
