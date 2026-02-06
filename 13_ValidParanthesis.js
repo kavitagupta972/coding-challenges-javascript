a69d0c23ef5c6ed61f44f5a00791284287e33713
kavita gupta <kavitagupta972@gmail.com>
2026-02-06 18:18:02 +0000

    Added solution for valid paranthesis

--- 13_ValidParanthesis.js
+++ 13_ValidParanthesis.js
@@ -3,3 +3,57 @@
 
 // Balanced: "[()()]{}" → every opening bracket is closed in the correct order.
 // Not balanced: "([{]})" → the ']' closes before the matching '{' is closed, breaking the nesting rule.
+function isValid(s) {
+    const stack = [];
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+    for (let char of s) {
+        // If closing bracket
+        if (map[char]) {
+            const top = stack.pop();
+            if (top !== map[char]) {
+                return false;
+            }
+        } else {
+            // Opening bracket
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
+}
+console.log(isValid("[{}]"));
+
+console.log(isValid("[{}"));
+
+
+function findValidParanthesis(str) {
+
+    const stack = [];
+    const map = {
+        '}' : '{',
+        ']' : '[',
+        ')' : '('
+    };
+
+    for(let char of str) {
+
+        if(map[char]) {
+            const top = stack.pop();
+            if(top !== map[char]){
+                return false;
+            }
+
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
+}
+
+console.log(findValidParanthesis("[()()]{}"));
+console.log(findValidParanthesis("([{]})"));
+
+console.log(findValidParanthesis("()"));
