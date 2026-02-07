ca6e23242a2c36146958ab9df623fb6b4d3a8262
kavita gupta <kavitagupta972@gmail.com>
2026-02-07 07:37:30 +0000

    Added containers for DSA problem of arrays

---
+++ 18_maxSumArr.js
@@ -0,0 +1,10 @@
+//Two Elements with Maximum Sum
+//
+
+function maxSum(arr) {
+
+
+
+}
+
+console.log(maxSum([20,30,40,50,60]));
---
+++ 19_maxDiffArr.js
@@ -0,0 +1,9 @@
+//Two Elements with Maximum Difference
+
+function maxDiff(arr) {
+
+
+
+}
+
+console.log(maxDiff([1,2,3,4,5,6,7,9,10]));
