e3c9b33258957306c82d20f8a411a32613b40c69
kavita gupta <kavitagupta972@gmail.com>
2026-02-07 05:28:49 +0000

    Added solution for movezero and repeateddigit

--- 14_MoveZeroes.js
+++ 14_MoveZeroes.js
@@ -1,3 +1,21 @@
 // The "Move Zeroes" problem is a common Data Structures and Algorithms (DSA) question that asks you to rearrange an array so all zeros are at the end, while maintaining the relative order of the non-zero elements, and doing so in-place (without using extra space). 
 // Input: [0, 1, 0, 3, 12]
 // Output: [1, 3, 12, 0, 0] 
+console.log("starting logic");
+function moveZero(arr) {
+var insertPos = 0;
+    for(let i = 0; i< arr.length; i++) {
+        if(arr[i] !== 0){
+            [arr[insertPos], arr[i]]=  [arr[i], arr[insertPos]];
+            insertPos++;
+        }
+
+    }
+
+    return arr;
+
+}
+
+console.log("moving zero", moveZero([1,0,3,0,6]));
+
+console.log("moving zero", moveZero([0, 1, 0, 3, 0]));
--- 15_findrepeatedDigit.js
+++ 15_findrepeatedDigit.js
@@ -8,4 +8,25 @@
 // Input: nums = [3,3,3,3,3]
 // Output: 3
 // https://www.geeksforgeeks.org/dsa/find-repetitive-element-1-n-1/
- 
+console.log("starting programme");
+var iterations = 0;
+ function findRepeatedDigit (arr) {
+    iterations = 0;
+    var items = new Set();
+    for(let num of arr){
+        iterations++;
+        if(items.has(num)) {
+            return num;
+        }
+        items.add(num);
+    }
+
+ }
+
+ console.log(findRepeatedDigit([1,3,4,2,2]));
+ console.log('number of iterations ', iterations);
+ console.log(findRepeatedDigit([3,1,6,4,6]));
+ console.log('number of iterations ', iterations);
+ console.log(findRepeatedDigit([3,3,3,3,3]));
+
+ console.log('number of iterations ', iterations);
