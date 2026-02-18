4d1dffc64fd1a4bdf0693ca85ed45838352e7251
kavita gupta <kavitagupta972@gmail.com>
2026-02-18 06:19:42 +0000

    Added more solutions

--- 20_strReverseInPlace.js
+++ 20_strReverseInPlace.js
@@ -1,1 +1,15 @@
-//dont use third variable
+//dont use third variable strings can not mutate. Since, they are immutable in nature
+
+
+// var str = "Hello, My Name is Kavita Gupta";
+var str = "Kavita"
+var str1 = str.split("");
+var i = 0; var j = str1.length-1;
+while(i<j){
+   
+   [str1[i], str1[j]] = [str1[j], str1[i]] ;
+   i++;
+   j--;
+}
+
+console.log(str1.join(""));
---
+++ 26_mergetwosortedarray.js
@@ -0,0 +1,3 @@
+// Example
+console.log(mergeSortedArrays([1,3,5], [2,4,6]));
+// Output: [1,2,3,4,5,6]
