75a443d1dc818254c8520acb8fc06bc3026da583
kavita gupta <kavitagupta972@gmail.com>
2026-05-04 06:03:08 +0000

    added buy sell stock leetcode problem

---
+++ 31_AlexMovie_Greedy_twopointer.js
@@ -0,0 +1,11 @@
+/**
+ * A friend of Alex has gifted them a movie collection, and 
+ * Alex is excited to watch them all as quickly as possible.
+ *  The duration of the movies is given in array durations[n],
+ *  where n is the number of movies, and each movie duration lies between 1.01 and 3.00 units of time 
+ * (up to two decimal places). Every day, Alex wants to spend no more than 3.00 units of time watching the movies
+ *  but also wants to complete the movies in the least number of days possible. Alex does not leave a movie in between. 
+ * That is, if Alex has picked up a movie, Alex watches the complete movie on the same day. Find the minimum number of days needed to watch all the movies. 
+ * This is a classic greedy + two-pointer problem (very similar to the “boats to save people” pattern).
+ */
+
